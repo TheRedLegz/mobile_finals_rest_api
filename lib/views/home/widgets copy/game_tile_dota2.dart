40065c2c93c7822ca75cbc7c060f4fb4ccc4fbc3
hubertrosal123 <rosalhubert78@gmail.com>
2021-07-26 23:17:05 +0800

    added dota 2 landingpage

--- lib/views/home/widgets copy/game_tile_dota2.dart
+++ lib/views/home/widgets copy/game_tile_dota2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mobile_finals_rest_api/views/home/dota2/dota_landing_page.dart';
 // import 'package:get/get.dart';
 
 class GameTileDota2 extends StatelessWidget {
@@ -14,30 +16,32 @@ class GameTileDota2 extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.grey[900],
-                    onPrimary: Colors.yellow,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey[900],
+                      onPrimary: Colors.yellow,
+                    ),
+                    child: Container(
+                        height: 180,
+                        width: double.infinity,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: Image.asset('images/dota2.jpg')),
+                    onPressed: () {
+                      Get.to(Dota2LandingPage());
+                    },
                   ),
-                  child: Container(
-                      height: 180,
-                      width: double.infinity,
-                      clipBehavior: Clip.antiAlias,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      child: Image.asset('images/dota2.jpg')),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
     );
   }
 }
