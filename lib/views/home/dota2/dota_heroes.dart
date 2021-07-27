78d7e7b770c9d9e580d89998895712513ec00ad0
hubertrosal123 <rosalhubert78@gmail.com>
2021-07-27 18:05:14 +0800

    display dota heroes

--- lib/views/home/dota2/dota_heroes.dart
+++ lib/views/home/dota2/dota_heroes.dart
@@ -1,13 +1,28 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
+import 'package:mobile_finals_rest_api/controllers/dota/dota_hero_list_controller.dart';
+import 'package:mobile_finals_rest_api/models/dota_models/dota_hero_data.dart';
+import 'package:mobile_finals_rest_api/services/test_service.dart';
+import 'package:mobile_finals_rest_api/views/home/dota2/widgets/heroes_card.dart';
 
 class DotaHeroes extends StatelessWidget {
-  // HeroListController heroListController = Get.put(HeroListController());
+  final DotaHeroListController _heroListController = Get.put(DotaHeroListController());
+  DotaHeroes({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
+        title: Text(
+          'Heroes',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
         backgroundColor: Colors.grey[900],
         elevation: 0,
         leading: IconButton(
@@ -17,42 +32,54 @@ class DotaHeroes extends StatelessWidget {
           },
         ),
       ),
-      body:
-          // Obx(
-          //   () =>
-          Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Heroes',
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Load Heroes',
                     style: TextStyle(
-                      fontSize: 32,
+                      fontSize: 12,
                       fontWeight: FontWeight.w900,
                     ),
                   ),
+                  IconButton(
+                    icon: Icon(Icons.refresh),
+                    onPressed: () async {
+                      var res = await TestService.fetchHeroList();
+                      dotaHeroDataListFromJson(res).forEach((hero) {
+                        print(hero.name);
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Obx(
+                  () => _heroListController.isLoading.value
+                      ? Center(child: CircularProgressIndicator())
+                      : StaggeredGridView.countBuilder(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 16,
+                          mainAxisSpacing: 16,
+                          itemCount: _heroListController.heroList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return DotaHeroesCard(
+                                _heroListController.heroList[index]);
+                          },
+                          staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                        ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          // Expanded(
-          //   child: StaggeredGridView.countBuilder(
-          //     crossAxisCount: 2,
-          //     itemCount: heroListController.heroList.length,
-          //     crossAxisSpacing: 16,
-          //     mainAxisSpacing: 16,
-          //     itemBuilder: (context, index) {
-          //       return DotaHeroesCard(heroListController.heroList[index]);
-          //     },
-          //     staggeredTileBuilder: (index) => StaggeredTile.fit(1),
-          //   ),
-          // )
-        ],
+        ),
       ),
-      // ),
     );
   }
 }
