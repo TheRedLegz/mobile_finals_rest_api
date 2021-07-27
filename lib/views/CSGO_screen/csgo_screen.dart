4c1b4a059cb83d3972c36602da196ac9e06ee662
augustorasmil <augustorasmil@gmail.com>
2021-07-27 13:26:38 +0800

    added csgo screen

---
+++ lib/views/CSGO_screen/csgo_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+
+class CSGOScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        title: Text('Counter-Strike Global Offensive'),
+      ),
+      body: Center(),
+    );
+  }
+}
