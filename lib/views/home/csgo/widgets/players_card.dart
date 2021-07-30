651faad94f909a8703bc2989822bfbe57cd4a553
augustorasmil <augustorasmil@gmail.com>
2021-07-30 10:13:52 +0800

    csgo cards

---
+++ lib/views/home/csgo/widgets/players_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_finals_rest_api/models/csgo_models/csgo_player_data.dart';
+
+class CsgoPlayerData extends StatelessWidget {
+  final CsgoPlayerData playerData;
+  CsgoPlayerData(this.playerData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Container(
+                    // height: 180,
+                    // width: double.infinity,
+                    // clipBehavior: Clip.antiAlias,
+                    // decoration: BoxDecoration(
+                    //   borderRadius: BorderRadius.circular(4),
+                    // ),
+                    // child: Image.network(
+                    //   matchData.imageUrl,
+                    //   fit: BoxFit.cover,
+                    // ),
+                    ),
+              ],
+            ),
+            SizedBox(height: 5),
+            Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                // children: [
+                //   Text(
+                //     " " ,
+                //     maxLines: 2,
+                //     style: TextStyle(fontSize: 12, fontWeight: FontWeight.w800),
+                //     overflow: TextOverflow.ellipsis,
+                //   ),
+                //   Text(
+                //     " " ,
+                //     maxLines: 2,
+                //     style: TextStyle(fontSize: 12, fontWeight: FontWeight.w800),
+                //     overflow: TextOverflow.ellipsis,
+                //   ),
+                // ],
+                ),
+            SizedBox(height: 8),
+          ],
+        ),
+      ),
+    );
+  }
+}
