8393bbadf34f12fb0e3be1f51ba50672f307a09a
augustorasmil <augustorasmil@gmail.com>
2021-08-01 10:23:09 +0800

    dota2 and csgo player controllers

---
+++ lib/controllers/csgo/csgo_player_list_controller.dart
@@ -0,0 +1,19 @@
+import 'package:get/get.dart';
+import 'package:mobile_finals_rest_api/models/pandascore_models/player_data.dart';
+import 'package:mobile_finals_rest_api/services/test_service.dart';
+
+class CsgoPlayerListController extends GetxController {
+  var csgoWeaponList = [].obs;
+  RxBool isLoading = true.obs;
+
+  void onInit() {
+    super.onInit();
+    fetchCsgoPlayerList();
+  }
+
+  void fetchCsgoPlayerList() async {
+    var responseBody = await TestService.fetchCsgoPlayerList();
+    isLoading.value = false;
+    csgoWeaponList.value = playerDataListFromJson(responseBody);
+  }
+}
