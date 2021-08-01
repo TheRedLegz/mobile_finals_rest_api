a2d3806ebd230dc593eb62a6af2acd73e22482df
TheRedLegz <redempto.legaspi.20@usjr.edu.ph>
2021-08-01 19:51:28 +0800

    Removed unused imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mobile_finals_rest_api/controllers/dota/dota_item_list_controller.dart';
-import 'package:mobile_finals_rest_api/models/dota_models/dota_hero_data.dart';
-import 'package:mobile_finals_rest_api/services/test_service.dart';
 import 'package:mobile_finals_rest_api/views/home/home.dart';
 
 void main() => runApp(MainApp());
